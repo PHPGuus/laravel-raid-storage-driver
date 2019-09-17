dd243e2c251868cf140aa7aeb2b452e664c23497
PHPGuus <php.guus@gmail.com>
2019-09-17 13:56:23 +0100

    Apply fixes from StyleCI (#1)

--- src/Console/RebuildRaidArray.php
+++ src/Console/RebuildRaidArray.php
@@ -11,36 +11,39 @@ class RebuildRaidArray extends Command
     //region Public Access
 
     /**
-     * Rebuild the RAID array underlying the given Storage disk name
+     * Rebuild the RAID array underlying the given Storage disk name.
      */
     public function handle()
     {
         $disk = $this->argument('disk');
-        if (!$disk) {
-            $this->error('Please provide the disk you want the RAID ' .
+        if (! $disk) {
+            $this->error('Please provide the disk you want the RAID '.
                 'configuration to be rebuilt for.');
+
             return;
         }
 
-        $diskConfig = config('filesystems.disks.' . $disk);
-        if (!$diskConfig) {
-            $this->error('Cannot find the filesystem configuration for disk "' .
-                $disk . '".');
+        $diskConfig = config('filesystems.disks.'.$disk);
+        if (! $diskConfig) {
+            $this->error('Cannot find the filesystem configuration for disk "'.
+                $disk.'".');
+
             return;
         }
 
         $fileSystems = [];
-        if(!isset($diskConfig['disks'])) {
-            $this->error('Please make sure you have a key "disks" in the ' .
-                'configuration of the disk "' . $disk . '".');
+        if (! isset($diskConfig['disks'])) {
+            $this->error('Please make sure you have a key "disks" in the '.
+                'configuration of the disk "'.$disk.'".');
+
             return;
         }
-        foreach($diskConfig['disks'] as $diskName) {
+        foreach ($diskConfig['disks'] as $diskName) {
             $fileSystems[] = Storage::disk($diskName);
         }
 
         $raidLevel = $diskConfig['raidLevel'] ?? 1;
-        if($raidLevel == 1) {
+        if ($raidLevel == 1) {
             $adapter = new RaidOneAdapter($fileSystems);
             $adapter->rebuildArray();
         }
@@ -62,7 +65,7 @@ class RebuildRaidArray extends Command
      *
      * @var string
      */
-    protected $signature = 'raid:rebuild-array {disk : The configured disk ' .
+    protected $signature = 'raid:rebuild-array {disk : The configured disk '.
         'for which to rebuild the array}';
 
     //endregion
